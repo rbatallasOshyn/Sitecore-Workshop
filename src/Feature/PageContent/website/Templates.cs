1a712f70ac51593267e51f070206758e58e9ef61
Ramiro Batallas <rbatallas@oshyn.com>
2024-01-01 22:55:52 -0500

    add footer logic

---
+++ src/Feature/PageContent/website/Templates.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Data;
+
+namespace Workshop.Feature.PageContent
+{
+    public static class Templates
+    {
+        public struct Footer
+        {
+            public static ID Id = new ID("{35ECC186-62E7-4BE1-8023-7DC6892A8724}");
+            public struct Fields
+            {
+                public static ID Content = new ID("{D1E12D54-4C24-4283-AB9D-A7FCB4CBD605}");
+            }
+        }
+    }
+}
